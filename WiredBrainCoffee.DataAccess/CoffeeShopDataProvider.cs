b7d98965d3882716e3b23c5946f5e7a2ea09d129
JohanElektor <johan@elektor-tecjnologies.co.za>
2022-12-26 23:17:21 +0200

    Added DataProvided and logic to main procedure to provide the help menu

---
+++ WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
@@ -0,0 +1,17 @@
+﻿using WiredBrainCoffee.DataAccess.Model;
+
+namespace WiredBrainCoffee.DataAccess
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShops()
+        {
+            return new List<CoffeeShop>()
+            {
+                new CoffeeShop() { Location = "Frankfurt", BeansInStockInKg = 107 },
+                new CoffeeShop() { Location = "Freiburg", BeansInStockInKg = 23 },
+                new CoffeeShop() { Location = "Munich", BeansInStockInKg = 56 }
+            };
+        }
+    }
+}
--- WiredBrainCoffee.ShopinfoToo/Program.cs
+++ WiredBrainCoffee.ShopinfoToo/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using WiredBrainCoffee.DataAccess;
+using WiredBrainCoffee.DataAccess.Model;
+
 Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+Console.WriteLine("Write 'help' to list available commands");
+var coffeeShopDataProvider = new CoffeeShopDataProvider();
+
+while (true)
+{
+    var line = Console.ReadLine();
+    var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
+    if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
+    {
+        foreach (CoffeeShop coffeeShop in coffeeShops)
+        {
+            Console.WriteLine($"> " + coffeeShop.Location);
+        }
+    }
+}
