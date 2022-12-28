9768819cf873124a792661bc7dc5b5b8fc7c52e9
JohanElektor <johan@elektor-tecjnologies.co.za>
2022-12-28 08:56:02 +0200

    Implemented Handling of Coffeeshop commands

--- WiredBrainCoffee.ShopinfoToo/Program.cs
+++ WiredBrainCoffee.ShopinfoToo/Program.cs
@@ -24,4 +24,26 @@ while (true)
             Console.WriteLine($"> " + coffeeShop.Location);
         }
     }
+    else
+    {
+        var foundCoffeeShops =coffeeShops.Where(x=>x.Location.StartsWith(line,StringComparison.OrdinalIgnoreCase)).ToList();
+        if (!foundCoffeeShops.Any()) 
+        {
+            Console.WriteLine($"> Command '{line}' not found!");
+        }
+        else if (foundCoffeeShops.Count() == 1)
+        {
+            var coffeeShop = foundCoffeeShops.Single();
+            Console.WriteLine($"> Location: {coffeeShop.Location}");
+            Console.WriteLine($"Beans in Stock: {coffeeShop.BeansInStockInKg} kg.");
+        }
+        else
+        {
+            Console.WriteLine($"Multiple matching coffee shop commands found:");
+            foreach(CoffeeShop coffeeShopFound in foundCoffeeShops )
+            {
+                Console.WriteLine($"> {coffeeShopFound.Location}");
+            }
+        }
+    }
 }
