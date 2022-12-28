0effe67b935f11f11e81354eda4020b30017e9b8
JohanElektor <johan@elektor-tecjnologies.co.za>
2022-12-28 11:56:35 +0200

    Converted the Program.cs to the Main method structure

--- WiredBrainCoffee.ShopinfoToo/Program.cs
+++ WiredBrainCoffee.ShopinfoToo/Program.cs
@@ -2,27 +2,33 @@
 using WiredBrainCoffee.DataAccess;
 using WiredBrainCoffee.DataAccess.Model;
 
-Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
-Console.WriteLine("Write 'help' to List available Coffee Shop Commands: Write 'Quit' to Exit the Application.");
-
-
-while (true)
+internal class Program
 {
-    var line = Console.ReadLine();
-    var coffeeShops = CoffeeShopDataProvider.LoadCoffeeShops();
-
-
-    // Break out of the while loop, if the user types 'Quit''
-    if (String.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
+    private static void Main(string[] args)
     {
-        break;
+        Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+        Console.WriteLine("Write 'help' to List available Coffee Shop Commands: Write 'Quit' to Exit the Application.");
+
+
+        while (true)
+        {
+            var line = Console.ReadLine();
+            var coffeeShops = CoffeeShopDataProvider.LoadCoffeeShops();
+
+
+            // Break out of the while loop, if the user types 'Quit''
+            if (string.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
+            {
+                break;
+            }
+
+            var commandHandler =
+                string.Equals("help", line, StringComparison.OrdinalIgnoreCase)
+                ? new HelpCommandHandler(coffeeShops) as ICommandHandler
+                : new CommandHandler(coffeeShops, line);
+
+
+            commandHandler.HandleCommand();
+        }
     }
-
-    var commandHandler =
-        String.Equals("help", line, StringComparison.OrdinalIgnoreCase)
-        ? new HelpCommandHandler(coffeeShops) as ICommandHandler
-        : new CommandHandler(coffeeShops, line);      
-    
-
-    commandHandler.HandleCommand();
 }
